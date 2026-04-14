463191047ac6c85ce80170e7ba68dd405c69a0e9
UpilMonster01 <rudicahyadi900@gmail.com>
2026-04-14 08:56:13 +0800

    update v1

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "budi";
+$umur = 20;
+$tinggi = 179.9;
+$hobi = ["membaca","berenang","bermain bola"];
+echo "Nama saya $nama,Umur saya $umur ,tinggi saya $tinggi,Hobi saya $hobi[2]"
+?>
