f839517276ad673b96e32cc91b4805779e8358a8
UpilMonster01 <rudicahyadi900@gmail.com>
2026-04-21 08:42:03 +0800

    update 1.2

--- index.php
+++ index.php
@@ -1,66 +1,11 @@
 <?php
-// variabel dan tipe data
-$nama = "budi";
-$umur = 20;
-$tinggi = 179.9;
-$hobi = ["membaca","berenang","bermain bola"];
-
-echo "Nama saya $nama, Umur saya $umur, tinggi saya $tinggi, Hobi saya $hobi[2]";
-echo "<br><br>===============================<br><br>";
-
-// operator dan kondisi (if else)
-
-//penjumlahan
-$nilai1 = 10;
-$nilai2 = 20;
-$hasil = $nilai1 + $nilai2;
-
-echo "Hasil  penjumlahan $nilai1 + $nilai2 = $hasil";
-echo "<br><br>===============================<br><br>";
-//pembagian
- 
-$nilai3 = 10;
-$nilai4 = 2;
-$hasil_bagi = $nilai3 / $nilai4;
-echo "Hasil  pembagian $nilai3 / $nilai4 = $hasil_bagi";
-echo "<br><br>===============================<br><br>";
-//perkalian
-
-$nilai5 = 10;
-$nilai6 = 5;
-$hasil_kali = $nilai5 * $nilai6;
-echo "Hasil  perkalian $nilai5 * $nilai6 = $hasil_kali";
-echo "<br><br>===============================<br><br>";
-//pengurangan
-
-$nilai7 = 10;
-$nilai8 = 5;
-$hasil_kurang = $nilai7 - $nilai8;
-echo "Hasil  pengurangan $nilai7 - $nilai8 = $hasil_kurang";
-echo "<br><br>===============================<br><br>";
-
-
-//pengkondisian
-$nilai9 = 90;
-if ($nilai9 >= 90) {
-    echo "Nilai A";
-} elseif ($nilai9 >= 80) {
-    echo "Nilai B";
-} elseif ($nilai9 >= 70) {
-    echo "Nilai C";
-} else {
-    echo "Nilai D";
-}
-
-//penentuan ganjil genap
-$nilai10 = 15;
-if ($nilai10 % 2 == 0) {
-    echo "<br><br>Nilai $nilai10 adalah genap";
-} else {
-    echo "<br><br>Nilai $nilai10 adalah ganjil";
-}
-echo "<br><br>===============================<br><br>";
-
-echo '<img src="https://www.pngmart.com/files/22/Meme-PNG-Photos.png" alt="Foto dari web" width="200">';
-
+echo "<br><button><a href='materi1.php'>Materi 1 variabel dan tipe data </a></button><br><br>";
+echo "<br><button><a href='materi2.php'>Materi 2 looping dan kondisi </a></button><br><br>";
+echo "<br><button><a href='materi3.php'>Materi 3</a></button><br><br>";
+echo "<br><button><a href='materi4.php'>Materi 4</a></button><br><br>";
+echo "<br><button><a href='materi5.php'>Materi 5</a></button><br><br>";
+echo "<br><button><a href='materi6.php'>Materi 6</a></button><br><br>";
+echo "<br><button><a href='materi7.php'>Materi 7</a></button><br><br>";
+echo "<br><button><a href='materi8.php'>Materi 8</a></button><br><br>";
+echo "<br><button><a href='materi9.php'>Materi 9</a></button><br><br>";
 ?>
---
+++ materi2.php
@@ -0,0 +1,18 @@
+<form method ="post" >
+    masukan angka :   <input type="number" name="angka"><br>
+    <input type="submit" value="kirim">
+</form>
+
+<?php
+if (isset($_POST['angka'])) {
+    $data=$_POST['angka'];  
+    for ($i=1; $i <= $data; $i++) {
+        echo "Angka: $i <br>";
+    }
+    if ($data % 2 == 0) {
+        echo "Angka $data adalah genap";
+    } else {
+        echo "Angka $data adalah ganjil";
+    }
+}
+ ?>
